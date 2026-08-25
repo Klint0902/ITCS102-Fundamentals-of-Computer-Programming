1d3fc3092ea4a46f1adae44a08d9dddcf42a9f05
Klint0902 <ingenteklint02@gmail.com>
2026-08-26 01:29:55 +0800

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,35 @@
+# using variables
+
+# int
+age = 171
+sibling_count = 4
+ex = 1 
+eyes = 2 
+zip_code = 4308
+
+# float 
+temp = 21.2
+money = 5.5
+change = 1.1
+height = 1.62
+weight = 46.2
+
+# string 
+username = "Gagamboy"
+password = "ako_may_sapot_ikaw_supot"
+motto = "Life_is_too_short_to_spend_it_at_war_with_yourself"
+favorite_drink = "Spanish_Latte"
+favorite_dish = "Sinigang"
+
+# boolean
+isRich = False
+isPoor = True 
+isFunny = True
+isBad = False 
+isGood = True 
+
+print("my username is, username")
+print("my password is, password")
+print("is klint pogi, isPogi")
+print("my motto in life is, motto")
+print("klint's favorite drink, favorite_drink")
