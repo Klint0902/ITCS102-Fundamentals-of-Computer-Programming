0a1f544561fdeb98cc3b07d9fb8e753ff2dd7c99
Klint0902 <ingenteklint02@gmail.com>
2026-08-26 02:09:42 +0800

    Add files via upload

---
+++ untitled-20260825175031.py
@@ -0,0 +1,14 @@
+#escape sequence 
+# \n = new line 
+# \t = tab
+# \r = carriage return\deprecated\obsolete
+# \b = backspace 
+# \\ = slash 
+# \" = double qoutation 
+
+phrase1 = "\"\t\tit is never\ntoo late\""
+phrase2 = "\"paalam lang \nang \\palagi\""
+
+
+print(phrase1)
+print(phrase2)
