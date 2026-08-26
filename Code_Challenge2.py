026da19d0a1377cf5c7b87668071132447e47541
Klint0902 <ingenteklint02@gmail.com>
2026-08-27 02:13:54 +0800

    Add files via upload
    worthhhhh ittt ang eyebagsssss wohooooo
    
    zo hapi :>

---
+++ Code_Challenge2.py
@@ -0,0 +1,39 @@
+Amount = 65215
+
+print("thousands =", Amount // 1000) 
+
+friv = Amount // 1000 * 1000
+
+print("five hundred/s =",  (Amount - friv) // 500)
+
+tuhan = (Amount - friv) % 500 
+
+print("two hundred/s =", tuhan // 200)
+
+wanhan = tuhan % 200
+
+print("one hundred/s =", wanhan // 100)
+
+pepti = wanhan % 100
+
+print("fifty/fifties =", pepti // 50) 
+
+venti = pepti % 50 
+
+print("twenty/twenties =", venti // 20)
+
+thins = venti % 20
+
+print("ten/s =", thins // 10)
+
+fight = thins % 10
+
+print("five/s =", fight // 5)
+
+juans = fight % 5 
+
+print("one/s =", juans // 1)
+
+
+
+
