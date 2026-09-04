16f963a81f0d660d4258724b81204f25a730078a
Klint0902 <ingenteklint02@gmail.com>
2026-09-04 16:14:10 +0800

    Add files via upload

---
+++ Activity7.py
@@ -0,0 +1,22 @@
+#Activity Operator
+#Activity7
+
+total = 2
+ 
+k = eval(input("Enter any number ----> "))
+total + k 
+ 
+l = eval(input("Enter any number ----> "))
+total += l 
+ 
+m  = eval(input("Enter any number ----> ")) 
+total *= m 
+ 
+n = eval(input("Enter any number ----> ")) 
+total -= n 
+ 
+o = eval(input("Enter any number ----> ")) 
+total /= o
+ 
+print("Current total =", total)
+ 
---
+++ Activity8.py
@@ -0,0 +1,36 @@
+#String operator
+
+hobbies = ""
+
+k = input("What are your hobbies? ")
+hobbies += k + ", "
+
+l = input("What else? ")
+hobbies += l + ", "
+
+m = input("Anything else? ")
+hobbies += m + ", "
+
+n = input("Anything else? ")
+hobbies += n + ", "
+
+o = input("Anything else? ")
+hobbies += o + ", "
+
+p = input("Anything else? ") 
+hobbies += p + ", "
+
+q = input("Anything else? ")
+hobbies += q + ", "
+
+r = input("Anything else? ")
+hobbies += q + ", "
+
+s = input("Anything else? ")
+hobbies += s + ", "
+
+t = input("Anything else? ")
+hobbies += t + "."
+
+
+print(hobbies)
