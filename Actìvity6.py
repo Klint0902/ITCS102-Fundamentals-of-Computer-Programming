6d91f66065eae2bc35c927c5ffe873051efb69d6
Klint0902 <ingenteklint02@gmail.com>
2026-09-04 16:51:45 +0800

    Add files via upload

---
+++ Actìvity6.py
@@ -0,0 +1,13 @@
+#Activity6
+#Overwrite
+#nakakagutom mag code 
+
+
+print("=================ADDING MACHINE==================")
+
+x = eval(input("Enter the value of 'x' ---> "))
+y = eval(input("Enter the value of 'y' ---> "))
+
+sum = x + y
+
+print("Answer : ", sum)
