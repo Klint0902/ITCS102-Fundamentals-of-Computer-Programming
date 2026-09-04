6c3aaa6612ece49a8ee43f5ada9d02048503dc24
Klint0902 <ingenteklint02@gmail.com>
2026-09-04 17:07:25 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#Activity6
+#Overwrite
+#nakakagutom mag code
+
+x = eval(input("Enter the value of 'x' -----> "))
+y = eval(input("Enter the value of 'y' -----> "))
+
+total = x + y 
+
+print("Answer: ", total)
