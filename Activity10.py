c6737cf90c1812d2a3bec449025396a23fc25b2e
Klint0902 <ingenteklint02@gmail.com>
2026-09-08 00:40:24 +0800

    Add files via upload

---
+++ Activity10.py
@@ -0,0 +1,13 @@
+#if else program 
+
+username = "clinton"
+password = "la la lost you"
+
+k = input("Input USERNAME ---> ")
+l = input("Input PASSWORD ---> ")
+
+if k == username or l == password:
+	print("username and password is correct")
+
+else:
+	print("lahat ng tao ay nagkakamali")
---
+++ Activity9.py
@@ -0,0 +1,20 @@
+k = eval(input("enter any number ----> "))
+l = eval(input("enter any number ----> "))
+
+#1
+print(k > l)
+
+#2
+print(k < l)
+
+#3
+print(k >= l)
+
+#4
+print(k <= l)
+
+#5
+print(k == l)
+
+#6
+print(k != l)
