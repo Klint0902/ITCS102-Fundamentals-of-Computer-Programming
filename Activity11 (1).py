cf2b356394f9161e269b006594adca8e3125e310
Klint0902 <ingenteklint02@gmail.com>
2026-09-08 00:59:59 +0800

    Add files via upload

---
+++ Activity11 (1).py
@@ -0,0 +1,13 @@
+import getpass
+
+username = "clinton"
+password = "la la lost you"
+
+k = input("Input USERNAME ---> ")
+l = getpass.getpass("Input PASSWORD ---> ")
+
+if k == username and l == password:
+        print("username and password is correct")
+
+else:
+        print("lahat ng tao ay nagkakamali")
